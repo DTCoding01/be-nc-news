d2aa4077ec8bc81b706d443700a8de4cf2cb27c5
Daniel Taylor <danieltaylor@Daniels-MacBook-Pro.local>
2024-07-15 15:20:58 +0100

    implemented get comments by article id and tests

--- models/articles.models.js
+++ models/articles.models.js
@@ -28,3 +28,19 @@ exports.fetchArticles = () => {
 exports.fetchArticleById = (article_id) => {
   return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id]);
 };
+
+exports.fetchArticleCommentsById = (article_id) => {
+  return db.query(
+    `SELECT 
+    comments.comment_id, 
+    comments.votes, 
+    comments.created_at, 
+    comments.author, 
+    comments.body,
+    comments.article_id
+  FROM comments
+  WHERE comments.article_id = $1
+  ORDER BY comments.created_at DESC`,
+    [article_id]
+  );
+};
