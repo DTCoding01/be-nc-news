a20f8559d5f970e7aa5ff5c6eeb9d4ed3de5cabe
Daniel Taylor <danieltaylor@Daniels-MacBook-Pro.local>
2024-07-18 10:39:52 +0100

    added routing and error handlers

--- app.js
+++ app.js
@@ -1,56 +1,22 @@
 const express = require("express");
-const app = express();
-const { getTopics } = require("./controllers/topics-controllers");
-const { getApi } = require("./controllers/api-controllers");
+const apiRouter = require("./routers/api-router");
 const {
-  getArticles,
-  getArticleById,
-  getArticleCommentsById,
-  postCommentToArticleId,
-  patchArticleById,
-} = require("./controllers/articles-controllers");
-const { deleteCommentById } = require("./controllers/comments-controllers");
-const { getUsers } = require("./controllers/users-controller");
+  handleInvalidInput,
+  handleCustomErrors,
+  handleInternalErrors,
+} = require("./error-handlers");
+const app = express();
 
 app.use(express.json());
 
-app.get("/api/topics", getTopics);
+app.use("/api", apiRouter);
 
-app.get("/api", getApi);
-
-app.get("/api/articles", getArticles);
-
-app.get("/api/articles/:article_id", getArticleById);
-
-app.get("/api/articles/:article_id/comments", getArticleCommentsById);
-
-app.get("/api/users", getUsers);
-
-app.post("/api/articles/:article_id/comments", postCommentToArticleId);
-
-app.patch("/api/articles/:article_id", patchArticleById);
-
-app.delete("/api/comments/:comment_id", deleteCommentById);
-
-app.all('*', (req, res) => {
+app.all("*", (req, res) => {
   res.status(404).send({ msg: "endpoint not found" });
 });
 
-app.use((err, req, res, next) => {
-  if (!err.code) {
-    return next(err);
-  }
-  if (err.code === "22P02") {
-    res.status(400).send({ msg: "invalid input" });
-  }
-});
-
-app.use((err, req, res, next) => {
-  if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg });
-  } else {
-    res.status(500).send({ msg: "internal server error" });
-  }
-});
+app.use(handleInvalidInput);
+app.use(handleCustomErrors);
+app.use(handleInternalErrors);
 
 module.exports = app;
